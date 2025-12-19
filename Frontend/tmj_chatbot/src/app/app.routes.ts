15a14e0756f02cb39385e20e55aacb456d5b8726
Arnav Naval <arnav.naval@gmail.com>
2025-12-18 19:48:00 -0800

    created skeleton frontend

--- Frontend/tmj_chatbot/src/app/app.routes.ts
+++ Frontend/tmj_chatbot/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'chat',
+    component: ChatComponent
+  },
+  {
+    path: '',
+    redirectTo: '/chat',
+    pathMatch: 'full'
+  }
+];
