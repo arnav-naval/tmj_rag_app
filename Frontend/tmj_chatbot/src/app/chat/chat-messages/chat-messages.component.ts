8d51b1435e4f22f41ae2ce6eb3b9071d1a22e0f9
Arnav Naval <arnav.naval@gmail.com>
2025-12-29 17:23:27 -0800

    improving chat ui

--- Frontend/tmj_chatbot/src/app/chat/chat-messages/chat-messages.component.ts
+++ Frontend/tmj_chatbot/src/app/chat/chat-messages/chat-messages.component.ts
@@ -13,5 +13,14 @@ export interface ChatMessage {
   styleUrl: './chat-messages.component.css'
 })
 export class ChatMessagesComponent {
-  messages = input<ChatMessage[]>([{ content: 'Hello! How can I assist you today?', role: 'bot' }, { content: 'I need help with my account.', role: 'user' }, { content: 'Sure! What seems to be the issue?', role: 'bot' }]);
+  //messages = input<ChatMessage[]>([]);
+  messages = input<ChatMessage[]>([{ content: 'Hello! How can I assist you today?', role: 'bot' },
+                                   { content: 'I need help with my account.', role: 'user' }, 
+                                   { content: 'Sure! What seems to be the issue?', role: 'bot' },
+                                   { content: 'I forgot my password.', role: 'user' },
+                                   { content: 'You can reset your password by clicking on "Forgot Password" on the login page.', role: 'bot' },
+                                   { content: 'Thank you!', role: 'user' },
+                                  { content: 'You\'re welcome! If you have any more questions, feel free to ask.', role: 'bot' },
+                                  { content: 'Great, thanks for your help!', role: 'user' },
+                                  { content: 'Anytime! Have a wonderful day!', role: 'bot' }]);
 }
