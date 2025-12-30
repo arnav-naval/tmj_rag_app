ae396a448be5614a055050a9e8f89e5d34f026a2
Arnav Naval <arnav.naval@gmail.com>
2025-12-29 21:36:49 -0800

    created chat input component

--- Frontend/tmj_chatbot/src/app/chat/chat-overview/chat-overview.component.ts
+++ Frontend/tmj_chatbot/src/app/chat/chat-overview/chat-overview.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChatHeaderComponent } from '../chat-header/chat-header.component';
 import { ChatMessagesComponent } from '../chat-messages/chat-messages.component';
+import { ChatInputComponent } from '../chat-input/chat-input.component';
 
 @Component({
   selector: 'app-chat-overview',
-  imports: [ ChatHeaderComponent, ChatMessagesComponent ],
+  imports: [ ChatHeaderComponent, ChatMessagesComponent, ChatInputComponent ],
   templateUrl: './chat-overview.component.html',
   styleUrl: './chat-overview.component.css'
 })
