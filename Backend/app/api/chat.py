9db3aaa79830b90eeac2fbc8aecf38fd7e778ccb
Arnav Naval <arnav.naval@gmail.com>
2026-01-04 14:39:19 -0800

    route w vector store injected working

--- Backend/app/api/chat.py
+++ Backend/app/api/chat.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.core.deps import Embeddings_Dep, VectorStore_Dep
+from langchain_pinecone import PineconeVectorStore
 
 router = APIRouter()
 
 @router.post("")
-async def chat(message: str):
-    return {"message": "Hello World"}
+async def chat(message: str, vectorstore: VectorStore_Dep):
+    docs = vectorstore.similarity_search(message, k=5)
+    return docs
--- Backend/app/core/deps.py
+++ Backend/app/core/deps.py
@@ -1,8 +1,10 @@
 from functools import lru_cache
+from langchain_core.vectorstores import VectorStore
 from app.core.settings import Settings
-from fastapi import Request
+from fastapi import Request, Depends
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from typing import Annotated
 
 @lru_cache
 def get_settings() -> Settings:
@@ -13,3 +15,13 @@ def get_vectorstore(request: Request) -> PineconeVectorStore:
 
 def get_embeddings(request: Request) -> OpenAIEmbeddings:
     return request.app.state.embeddings
+
+Embeddings_Dep = Annotated[
+    OpenAIEmbeddings,
+    Depends(get_embeddings),
+]
+
+VectorStore_Dep = Annotated[
+    VectorStore,
+    Depends(get_vectorstore),
+]
