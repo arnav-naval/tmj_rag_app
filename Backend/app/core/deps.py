6109853e78f473e7d48a4c6a897e7779e406f55f
Arnav Naval <arnav.naval@gmail.com>
2026-01-04 14:17:05 -0800

    added DI for vector store and embeddings

--- Backend/app/core/deps.py
+++ Backend/app/core/deps.py
@@ -1,6 +1,15 @@
 from functools import lru_cache
 from app.core.settings import Settings
+from fastapi import Request
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 @lru_cache
 def get_settings() -> Settings:
     return Settings()
+
+def get_vectorstore(request: Request) -> PineconeVectorStore:
+    return request.app.state.vectorstore
+
+def get_embeddings(request: Request) -> OpenAIEmbeddings:
+    return request.app.state.embeddings
